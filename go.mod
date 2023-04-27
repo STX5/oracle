63614774ab5dd8e224ac0c48f34edf4f7f5e3937
STX5 <stormtrooperx555@gmail.com>
2023-04-27 21:49:03 +0800

    添加docker-compose文件，更新任务完成情况

--- go.mod
+++ go.mod
@@ -9,11 +9,14 @@ require (
 
 require (
 	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-ole/go-ole v1.2.1 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/holiman/uint256 v1.2.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
@@ -24,6 +27,7 @@ require (
 )
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.1
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -34,7 +38,7 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
-	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
--- src/worker/job.go
+++ src/worker/job.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
@@ -24,38 +25,36 @@ type JobVal struct {
 
 // TODO: add timeout
 func (j Job) Scrap() (string, error) {
-    log.Println("start scraping")
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
+	log.Println("start scraping")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-    ch := make(chan string, 1)
-    errCh := make(chan error, 1)
-    go func() {
-        res, err := http.Get(j.URL)
-        if err != nil {
-            errCh <- err
-            return
-        }
-        data, err := j.resolve(res)
-        if err != nil {
-            errCh <- err
-            return
-        }
-        ch <- data
-    }()
+	ch := make(chan string, 1)
+	errCh := make(chan error, 1)
+	go func() {
+		res, err := http.Get(j.URL)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		data, err := j.resolve(res)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		ch <- data
+	}()
 
-    select {
-    case data := <-ch:
-        return data, nil
-    case err := <-errCh:
-        return "", err
-    case <-ctx.Done():
-        return "", ctx.Err()
-    }
+	select {
+	case data := <-ch:
+		return data, nil
+	case err := <-errCh:
+		return "", err
+	case <-ctx.Done():
+		return "", ctx.Err()
+	}
 }
 
-// Not implemented
-// TODO: add resolver
 func (j Job) resolve(resp *http.Response) (string, error) {
 	defer resp.Body.Close()
 	//body, err := io.ReadAll(resp.Body)
@@ -66,9 +65,8 @@ func (j Job) resolve(resp *http.Response) (string, error) {
 	var result string
 	dom.Find(j.Pattern).Each(func(i int, selection *goquery.Selection) {
 		result = selection.Text()
-		
+
 	})
 	return result, nil
-	// TODO: ADD RESOLVER
 	//return string(body), nil
 }
