e5faaf074e95a04be947251a3755e717144df86a
西城风雨楼 <aiker67@163.com>
2023-05-15 09:52:12 +0800

    修改了测试样例

--- smartContract/oracle_test.go
+++ smartContract/oracle_test.go
@@ -2,128 +2,32 @@ package smartcontract
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"github.com/ethereum/go-ethereum/crypto"
-	"io"
 	"log"
 	"testing"
 )
 
-import (
-	"bytes"
-	"net/http"
-)
-
-type ethRequest struct {
-	general
-	Method string        `json:"method"`
-	Params []interface{} `json:"params"`
-}
-
-type general struct {
-	JsonRPC string `json:"jsonrpc"`
-	ID      int    `json:"id"`
-}
-
-type listWalletsResult struct {
-	Jsonrpc string `json:"jsonrpc"`
-	Id      int    `json:"id"`
-	Result  []struct {
-		Url      string `json:"url"`
-		Status   string `json:"status"`
-		Accounts []struct {
-			Address string `json:"address"`
-			Url     string `json:"url"`
-		} `json:"accounts"`
-	} `json:"result"`
-}
-
-func TestNewOracle(t *testing.T) {
-	// 获取私钥，该私钥是oracle的私钥
-	privateKey, err := crypto.HexToECDSA("")
-	if err != nil {
-	}
-	err, publicKeyAddress := getPublicKeyAddress(privateKey)
-	if err != nil {
-	}
-
+func TestLockAndUnlockAccount(t *testing.T) {
+	unlockRequest := "{\"jsonrpc\":\"2.0\",\"method\":\"personal_unlockAccount\",\"params\":[\"0x8Ff1EBd1639dF7ca2FCF67eAcDC7488d567ec040\", \"cs237239\", 30],\"id\":1}"
+	lockRequest := "{\"jsonrpc\":\"2.0\",\"method\":\"personal_lockAccount\",\"params\":[\"0x8Ff1EBd1639dF7ca2FCF67eAcDC7488d567ec040\"],\"id\":1}"
 	listWalletRequest := "{\"jsonrpc\":\"2.0\",\"method\":\"personal_listWallets\",\"params\":[],\"id\":1}"
-	listWalletResultBytes, err := invokeJsonRpc("", []byte(listWalletRequest))
+	result, err := invokeJsonRpc("http://127.0.0.1:8545", []byte(unlockRequest))
 	if err != nil {
+		logger.Fatal("解锁账户失败")
 	}
+	logger.Println(string(result))
 
-	listWalletResult := new(listWalletsResult)
-	err = json.Unmarshal(listWalletResultBytes, listWalletResult)
+	result, err = invokeJsonRpc("http://127.0.0.1:8545", []byte(lockRequest))
 	if err != nil {
+		logger.Fatal("锁定账户失败")
 	}
+	logger.Println(string(result))
 
-	isLocked := true
-	for _, result := range listWalletResult.Result {
-		for _, account := range result.Accounts {
-			if account.Address == publicKeyAddress.Hex() {
-				if result.Status == "Locked" {
-					isLocked = true
-					break
-				}
-			}
-		}
-	}
-
-	if !isLocked {
-		// 没有被锁住，那么就不进行任何操作
-	}
-}
-
-// 生成ethRequest，Marshal 成[]byte ，传入do函数即可操作ethereum 节点
-func do(jsonParam []byte) ([]byte, error) {
-	reader := bytes.NewReader(jsonParam)
-	url := "http://127.0.0.1:8545"
-	request, err := http.NewRequest("POST", url, reader)
+	result, err = invokeJsonRpc("http://127.0.0.1:8545", []byte(listWalletRequest))
 	if err != nil {
-		return []byte(""), err
+		logger.Fatal("查询钱包数据失败")
 	}
-	request.Header.Set("Content-Type", "application/json")
-	client := http.Client{}
-	resp, err := client.Do(request)
-	if err != nil {
-		return []byte(""), err
-	}
-	respBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return []byte(""), err
-	}
-	return respBytes, nil
-}
-
-func TestJsonRpc(t *testing.T) {
-	//unlockRequest := "{\"jsonrpc\":\"2.0\",\"method\":\"personal_unlockAccount\",\"params\":[\"0x8Ff1EBd1639dF7ca2FCF67eAcDC7488d567ec040\", \"cs237239\", 30],\"id\":1}"
-	//unlockRequest := "{\"jsonrpc\":\"2.0\",\"method\":\"personal_unlockAccount\",\"params\":[\"0x8Ff1EBd1639dF7ca2FCF67eAcDC7488d567ec040\", \"cs237239\", 30]}"
-	//lockRequest := "{\"jsonrpc\":\"2.0\",\"method\":\"personal_lockAccount\",\"params\":[\"0x8Ff1EBd1639dF7ca2FCF67eAcDC7488d567ec040\"],\"id\":1}"
-	//listAccounts := "{\"jsonrpc\":\"2.0\",\"method\":\"personal_listWallets\",\"params\":[],\"id\":1}"
-	importRawKey := "{\"jsonrpc\":\"2.0\",\"method\":\"personal_importRawKey\",\"params\":[\"0x8Ff1EBd1639dF7ca2FCF67eAcDC7488d567ec040\", \"cs237239\"],\"id\":1}"
-	//isLockFormat := "{\"jsonrpc\":\"2.0\",\"method\":\"personal_listWallets\",\"params\":[]}"
-	i, err := do([]byte(importRawKey))
-	if err != nil {
-		fmt.Println(err)
-	}
-	//result := new(listWalletsResult)
-	//bytes, err := do([]byte(importRawKey))
-	//err = json.Unmarshal(bytes, result)
-	//fmt.Println(err)
-	//fmt.Println(result)
-	//privateKey, err := crypto.HexToECDSA("9e082d9aa6240a7af133616ac9f740aa67ba72b0372a366ac9890bb6e9740321")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//status := result.Result[0].Status
-	//fmt.Println(status)
-	//// 获得当前私钥对应的公钥
-	//publicKey := privateKey.Public()
-	//publicKeyECDSA, _ := publicKey.(*ecdsa.PublicKey)
-	//address := crypto.PubkeyToAddress(*publicKeyECDSA)
-	//fmt.Println(address == common.HexToAddress("0x8Ff1EBd1639dF7ca2FCF67eAcDC7488d567ec040"))
-	fmt.Println(string(i))
+	logger.Println(string(result))
 }
 
 // 测试Oracle的使用
@@ -136,7 +40,7 @@ func TestOracleWriter(t *testing.T) {
 func TestWrite(t *testing.T) {
 	oracle := NewOracle()
 	taskMap.jobMap["0xca5b322c08d498403051304c4c9ec9092bdeaf381b0804ba1e6c6227c55721cc"] = "0x8Ff1EBd1639dF7ca2FCF67eAcDC7488d567ec040"
-	ok, err := oracle.WriteData("0xca5b322c08d498403051304c4c9ec9092bdeaf381b0804ba1e6c6227c55721cc", "hello,oracle")
+	ok, err := oracle.WriteData("0xca5b322c08d498403051304c4c9ec9092bdeaf381b0804ba1e6c6227c55721cc", "测试数据")
 	if err != nil {
 		log.Fatal("写入错误", err)
 	}
@@ -145,6 +49,7 @@ func TestWrite(t *testing.T) {
 	} else {
 		fmt.Println("写入失败")
 	}
+	select {}
 }
 
 func TestQueryEtcd(t *testing.T) {
